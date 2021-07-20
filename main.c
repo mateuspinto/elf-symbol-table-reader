be56e3dccc4e4ac126682d5bcf486bf3a2ba6d43
Mateus Pinto da Silva <mateusps10@outlook.com>
2021-07-20 15:38:15 -0300

    changed to clang

--- main.c
+++ main.c
@@ -1,102 +1,103 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <libelf.h>
 #include <gelf.h>
+#include <libelf.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-    Elf         *elf;
-    Elf_Scn     *scn = NULL;
-    GElf_Shdr   shdr;
-    Elf_Data    *data;
-    GElf_Sym sym;
-    int         fd, ii, count;
-    long unsigned int iterator_print = 0;
+int main(int argc, char **argv) {
+  Elf *elf;
+  Elf_Scn *scn = NULL;
+  GElf_Shdr shdr;
+  Elf_Data *data;
+  GElf_Sym sym;
+  int fd, ii, count;
+  long unsigned int iterator_print = 0;
 
-    if(argc==1) {
-        printf("Please input the elf file path in args!\n");
-        return 1;
+  if (argc == 1) {
+    printf("Please input the elf file path in args!\n");
+    return 1;
+  }
+
+  elf_version(EV_CURRENT);
+
+  fd = open(argv[1], O_RDONLY);
+  elf = elf_begin(fd, ELF_C_READ, NULL);
+
+  while ((scn = elf_nextscn(elf, scn)) != NULL) {
+    gelf_getshdr(scn, &shdr);
+    if (shdr.sh_type == SHT_SYMTAB) {
+      /* found a symbol table, go print it. */
+      break;
+    }
+  }
+
+  data = elf_getdata(scn, NULL);
+  count = shdr.sh_size / shdr.sh_entsize;
+
+  /* print the symbol names */
+  for (ii = 0; ii < count; ++ii) {
+
+    gelf_getsym(data, ii, &sym);
+
+    printf("%lu: ", iterator_print);
+    iterator_print++;
+
+    // value
+    printf("0x%.8lu", sym.st_value);
+
+    // size
+    printf(" 0x%.8lu", sym.st_size);
+
+    // type
+    switch (sym.st_info) {
+    case 0:
+      printf("  NOTY");
+      break;
+    case 1:
+      printf("  OBJT");
+      break;
+    case 2:
+      printf("  FUNC");
+      break;
+    case 3:
+      printf("  SECT");
+      break;
+    case 4:
+      printf("  FILE");
+      break;
+
+    default:
+      break;
     }
 
-    elf_version(EV_CURRENT);
+    // bind
+    switch (sym.st_info) {
+    case 0:
+      printf(" LOCL");
+      break;
+    case 1:
+      printf(" GLOB");
+      break;
+    case 2:
+      printf(" WEAK");
+      break;
+    case 3:
+      printf("  NUM");
+      break;
 
-    fd = open(argv[1], O_RDONLY);
-    elf = elf_begin(fd, ELF_C_READ, NULL);
-
-    while ((scn = elf_nextscn(elf, scn)) != NULL) {
-        gelf_getshdr(scn, &shdr);
-        if (shdr.sh_type == SHT_SYMTAB) {
-            /* found a symbol table, go print it. */
-            break;
-        }
+    default:
+      break;
     }
 
-    data = elf_getdata(scn, NULL);
-    count = shdr.sh_size / shdr.sh_entsize;
+    // name
+    printf(" %s", elf_strptr(elf, shdr.sh_link, sym.st_name));
 
-    /* print the symbol names */
-    for (ii = 0; ii < count; ++ii) {
+    printf("\n");
+  }
+  elf_end(elf);
+  close(fd);
 
-        gelf_getsym(data, ii, &sym);
-
-        printf("%lu: ", iterator_print);
-        iterator_print++;
-
-        //value
-        printf("0x%.8lu", sym.st_value);
-
-        //size
-        printf(" 0x%.8lu", sym.st_size);
-
-        //type
-        switch (sym.st_info) {
-            case 0:
-                printf("  NOTY");
-                break;
-            case 1:
-                printf("  OBJT");
-                break;
-            case 2:
-                printf("  FUNC");
-                break;
-            case 3:
-                printf("  SECT");
-                break;
-            case 4:
-                printf("  FILE");
-                break;
-
-            default:
-                break;
-        }
-
-        //bind
-        switch(sym.st_info)
-        {
-            case 0: printf(" LOCL");
-                break;
-            case 1: printf(" GLOB");
-                break;
-            case 2: printf(" WEAK");
-                break;
-            case 3: printf("  NUM");
-                break;
-
-            default:
-                break;
-        }
-
-        //name
-        printf(" %s", elf_strptr(elf, shdr.sh_link, sym.st_name));
-
-        printf("\n");
-        
-    }
-    elf_end(elf);
-    close(fd);
-
-    return 0;
+  return 0;
 }
--- makefile
+++ makefile
@@ -1,28 +1,15 @@
-CC      := $(CROSS_COMPILE)cc -xc
-CXX     := $(CROSS_COMPILE)cc -xc++
-AS      := $(CROSS_COMPILE)cc
-AR      := $(CROSS_COMPILE)ar
-LD      := $(CROSS_COMPILE)g++
-NM      := $(CROSS_COMPILE)nm
-STRIP   := $(CROSS_COMPILE)strip
-RM      := rm -f
-CP      := cp -f
-OBJDUMP := $(CROSS_COMPILE)objdump
-OBJCOPY := $(CROSS_COMPILE)objcopy
+BIN=elf-reader
+SOURCE=main.c
 
-MKDIR 	:= mkdir -p
-ECHO	:= /bin/echo
-
-LFLAGS :=-lelf
-CFLAGS :=-O2 -march=native
-
-export CROSS_COMPILE
+CC=clang
+LFLAGS:=-lelf -flto
+CFLAGS:=-O3 -march=native
 
 all:
-	$(AS) main.c -o elf-reader $(CFLAGS) $(LFLAGS)
+	$(CC) $(SOURCE) -o $(BIN) $(CFLAGS) $(LFLAGS)
 
-test:
-	@$(MAKE) --no-print-directory all && ./elf-reader elf-reader
+run:
+	./$(BIN)
 
 clean:
-	rm -f elf-reader
+	rm $(BIN)
